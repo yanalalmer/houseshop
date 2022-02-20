8d40fccd5fda4b4d5ff7322a1d4aa352cdbb7d54
Yanal Almir <yanalalmir@Yanals-Mac-Pro.local>
2022-02-20 09:03:19 +0100

    home page into smaller components and styling menu item components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/HomePage';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/Directroy.jsx
+++ src/components/directory/Directroy.jsx
@@ -9,7 +9,7 @@ class Directory extends React.Component {
       sections: [
         {
           title: 'hats',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
         },
         {
@@ -19,18 +19,18 @@ class Directory extends React.Component {
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
         },
         {
           title: 'womens',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
         },
         {
           title: 'mens',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
         },
