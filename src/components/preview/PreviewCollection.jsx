674480e08d6e6281b86c6caecd2babb9b6bf64bd
Yanal Almir <yanalalmir@Yanals-Mac-Pro.local>
2022-02-20 10:14:05 +0100

    added shoppage, collection preview and collection item

--- src/components/preview/PreviewCollection.jsx
+++ src/components/preview/PreviewCollection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './PreviewCollection.scss';
+import CollectionItem from '../collectionitem/CollectionItem';
 
 const PreviewCollection = ({ title, items }) => (
   <div className='preview-collection'>
@@ -7,8 +8,8 @@ const PreviewCollection = ({ title, items }) => (
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
-        .map((item) => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...itemProps }) => (
+          <CollectionItem key={id} {...itemProps} />
         ))}
     </div>
   </div>
